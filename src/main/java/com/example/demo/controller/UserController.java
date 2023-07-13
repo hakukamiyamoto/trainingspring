5ea70100626c2aa41d7f92b14c834c17da04bae4
rika kohda <fukuohrika00@gmail.com>
2023-07-13 11:41:48 +0900

    step4の課題です。

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+/**
+ * ユーザー情報 Controller
+ */
+@Controller
+public class UserController {
+
+	/**
+	 * ユーザー情報 Service
+	 */
+	@Autowired
+	UserService userService;
+
+	/**
+	 * ユーザー情報一覧画面を表示
+	 * @param model Model
+	 * @return ユーザー情報一覧画面のHTML
+	 */
+	@GetMapping("/user/list")
+	public String displayList(Model model) {
+		List<User> userlist = userService.searchAll();
+		model.addAttribute("userlist", userlist);
+		return "user/list";
+	}
+}
