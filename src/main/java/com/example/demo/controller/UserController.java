bbebdc4100e2e41c2419843c2ba0985a858b3c37
rika kohda <fukuohrika00@gmail.com>
2023-07-25 13:25:45 +0900

    コメントを修正しました。間違えて変なところでマージしてしまいました。すみません。

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -286,7 +286,6 @@ public class UserController {
 			// ファイルの内容を読み取るためのInputStreamを作成
 			BufferedReader in = new BufferedReader(new InputStreamReader(file.getInputStream(), "Shift_JIS"));
 
-			// CSVファイルをパースし、各レコードを取得
 			Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
 
 			// 取得した全てのレコードに対して登録
