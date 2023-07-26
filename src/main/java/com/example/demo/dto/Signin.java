b93b4070c4b0f3ab44afb71b7fe87693124c2470
rika kohda <fukuohrika00@gmail.com>
2023-07-26 13:02:46 +0900

    ログインページを作成しました

---
+++ src/main/java/com/example/demo/dto/Signin.java
@@ -0,0 +1,27 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+/**
+ * ユーザー情報 リクエストデータ
+ */
+@Data
+public class Signin implements Serializable {
+	/**
+	 * id
+	 */
+	@NotEmpty(message = "IDを入力してください")
+	@Size(max = 100, message = "IDは100桁以内で入力してください")
+	private Long id;
+	/**
+	 * パスワード
+	 */
+	@NotEmpty(message = "パスワードを入力してください")
+	@Size(max = 255, message = "パスワードは255桁以内で入力してください")
+	private String password;
+}
